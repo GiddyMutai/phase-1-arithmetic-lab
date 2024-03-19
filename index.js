67ee7f20d3f8f26edf94c03d09bb80e9d2e865db
Gideon Mutai <giddymutai13@gmail.com>
2024-03-19 17:29:11 +0300

    Adds arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// Multiplication equation whose result is 62
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1*num2
+
+// Generates a random integer greater than 0
+const random = Math.floor((Math.random() * 10 ) + 1) 
+
+// Remainder to equal 4
+const num3 = 18
+const num4 = 7
+
+const mod = num3%num4
+
+// Returns 20 as the maximum number
+const list = (6,9,8,7,20)
+
+const max = Math.max(list)
